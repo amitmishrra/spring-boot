5b435b18ec393c3448dec6339896234a768d690c
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2023-01-02 01:19:18 +0530

    get mapping

--- src/main/java/com/spring/Application/Controller/ControllerClass.java
+++ src/main/java/com/spring/Application/Controller/ControllerClass.java
@@ -1,9 +1,7 @@
 package com.spring.Application.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ControllerClass {
